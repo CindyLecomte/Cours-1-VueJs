4b5f0273209715c6ef9c9e822401dbe5aa88c801
Cindy <cindy.lecomte59@gmail.com>
2024-03-27 11:53:19 +0100

    Créer un composant

--- VueJs/src/App.vue
+++ VueJs/src/App.vue
@@ -1,10 +1,17 @@
+<template>
+
+<HelloWorld /> <!--On effectue le rendu de notre composant avec un self-closing tag-->
+
+</template>
+
+
 <script setup>
+
+import HelloWorld from './components/HelloWorld.vue'; // On importe notre composant HelloWorld
 
 </script>
 
-<template>
 
-</template>
 
 <style scoped>
 
---
+++ VueJs/src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Hello World</h1> <!--On ajoute un titre-->
+</template>
+
+<script setup>
+
+</script>
