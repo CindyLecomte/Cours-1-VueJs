b1df052ec32adb598d4f504e1b225b7cf06ccdee
Cindy <cindy.lecomte59@gmail.com>
2024-03-27 15:45:51 +0100

    première utilisation de ref

--- VueJs/src/App.vue
+++ VueJs/src/App.vue
@@ -2,7 +2,7 @@
 
 <HelloWorld :sub-title="subTitle" /> <!--On passe notre variable au composant parent, avec uns syntaxe en kebab-case.-->
 
-
+<input type="text" v-model="subTitle"> <!--On crée un input texte. On lie la variable à l'input avec v-model. Cela permet de lier un input à une variable. -->
 
 </template>
 
@@ -10,10 +10,12 @@
 <script setup>
 
 import HelloWorld from './components/HelloWorld.vue'; // On importe notre composant HelloWorld
+import { ref } from 'vue';
 
 
+const subTitle = ref(""); // On définit que notre variable subTitle utilise ref. Ce qui va faire en sorte qu'au changement dans l'input, la valeur va changer. On met une chaîne de caractère vide par défaut, mais si on change la valeur dans le navigateur, cela fonctionnera.
 
-const subTitle = "Bonjour ! " // On crée une variable appelée subTitle, dans laquelle on indique le contenu qu'on veut.
+
 
 </script>
 
