4f581168455cda010466a97d88f02a644ba3ae84
Cindy <cindy.lecomte59@gmail.com>
2024-03-28 08:58:02 +0100

    v-if

--- VueJs/src/App.vue
+++ VueJs/src/App.vue
@@ -2,6 +2,16 @@
 
 <HelloWorld :user="user" /> <!--On passe à notre composant notre nouvelle variable, user-->
 
+
+<div
+
+v-if="user.age > 15" 
+> <!--On crée une condition avec v-if : si user.age est strictement supérieur à 15, Mon âge sera afifché. -->
+Mon âge <!--On crée une div dans laquelle on inscrit Mon âge-->
+
+</div>
+
+
 <input type="text" v-model="user.name"> <!--On crée deux nouveaux inpout, un qui va être lié au name-->
 <input type="text" v-model="user.age">  <!--Et un qui va être lié a age. On doit mettre user. devant, car c'est un objet.-->
 
