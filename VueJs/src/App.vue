688eba322f237d87a7d2b1ab49528d3652a9b4e7
Cindy <cindy.lecomte59@gmail.com>
2024-03-28 09:19:05 +0100

    ajout de style avec condition

--- VueJs/src/App.vue
+++ VueJs/src/App.vue
@@ -1,11 +1,15 @@
 <template>
 
-<HelloWorld :user="user" /> <!--On passe à notre composant notre nouvelle variable, user-->
+<HelloWorld /> 
 
 
+<div
+:class="[user.age > 30 ? 'text-red' : 'text-green']"
+>   <!--On attribue une class à cette div en faisant une condition en ternaire : si l'âge de l'utilisateur est strictement supérieur à 30, le nom sera en rouge. Sinon, le nom sera en vert. Cela changera si on change l'âge dans notre const user.-->
 
-<input type="text" v-model="user.name" v-on:input="display"> <!--On peut greffer des choses au niveau des événements. On utilise v-on sur input du name. -->
-<input type="text" v-model="user.age">  <!--Et un qui va être lié a age. On doit mettre user. devant, car c'est un objet.-->
+    {{ user.name }} <!--On crée une div dans laquelle on va afficher le nom du user-->
+
+</div>
 
 
 </template>
@@ -18,14 +22,10 @@ import { reactive } from 'vue'; //On importe reactive
 
 const user = reactive ({ // Au lieu de ref, on met reactive
     name : 'Cindy',
-    age: 26,    // On crée une nouvelle constante en lui passant un objet, avec le name et l'age.
+    age: 31,    // On crée une nouvelle constante en lui passant un objet, avec le name et l'age.
 })
 
-const display = () => { 
-    console.log(user.name, user.age); // On crée une fonction display, où on va afficher en console le nom et l'âge. Puisqu'on a mis v-on sur le name, on aura un changement en console à chaque modification de l'input name. (mais pas de l'âge)
-}
 
-display() //On oublie pas d'appeler la fonction
 
 </script>
 
@@ -34,5 +34,13 @@ display() //On oublie pas d'appeler la fonction
 
 
 <style scoped>
+
+.text-red {
+    color: red;
+}
+
+.text-green {
+    color: green; /* On crée une classe pour une couleur rouge et une classe pour la couleur verte*/
+}
 
 </style>
--- VueJs/src/components/HelloWorld.vue
+++ VueJs/src/components/HelloWorld.vue
@@ -1,13 +1,13 @@
 <template>
 
 <h1>Hello World</h1>
-<p>{{ props.user.name }} {{ props.user.age }}</p> <!--On affiche le nom de l'user ainsi que l'âge du user.-->
+
 
 
 </template>
 
 <script setup>
 
-const props = defineProps(['user']); // On change defineProps, en lui passant cette fois user.
+
 
 </script>
